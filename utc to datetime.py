5d738ce2f569b4c07efdd0d0e505898152d07388
eskaerpe <83657940+eskaerpe@users.noreply.github.com>
2021-11-29 16:50:30 +0700

    Adding the converter file
    uh, what

---
+++ utc to datetime.py
@@ -0,0 +1,16 @@
+from datetime import datetime #import the datetime module
+
+# declare your UTC+ number
+myUTC = int(7) #Change this number on this line depends your region
+
+# time UTC+0
+utcnow = datetime.utcnow()
+
+#add the hour number from the "utcnow" variable with your UTC Time (myUTC)
+myHour = int(datetime.strftime(utcnow,"%H")) + myUTC
+
+date = datetime.strftime(utcnow, "Date : %A, %d %B %Y") # the output is : weekday, day of the month, month, year
+time = datetime.strftime(utcnow, f"Time : {myHour}:%y")
+
+print(date)
+print(time)
