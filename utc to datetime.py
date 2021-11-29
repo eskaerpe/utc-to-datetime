0c9fe28bfe7b25c8a5060d24221f128f6ff3026c
eskaerpe <83657940+eskaerpe@users.noreply.github.com>
2021-11-29 17:01:00 +0700

    fixed
    just adding a comment LOL

--- utc to datetime.py
+++ utc to datetime.py
@@ -9,6 +9,7 @@ utcnow = datetime.utcnow()
 #add the hour number from the "utcnow" variable with your UTC Time (myUTC)
 myHour = int(datetime.strftime(utcnow,"%H")) + myUTC
 
+# get the data
 date = datetime.strftime(utcnow, "Date : %A, %d %B %Y") 
 time = datetime.strftime(utcnow, f"Time : {myHour}:%y")
 
